6a90b4d31e2dc5562a30dd8da124d890dad6a277
Jacob Lifshay <programmerjake@gmail.com>
2016-03-29 18:41:41 -0600

    working on changing object format

--- javascript_tasklets/gc.h
+++ javascript_tasklets/gc.h
@@ -19,54 +19,79 @@ namespace gc
 {
 struct ObjectDescriptorBase;
 
-struct Object
-{
-    const ObjectDescriptorBase *objectDescriptor;
-    explicit constexpr Object(const ObjectDescriptorBase *objectDescriptor);
-};
+struct Object;
 
 struct ObjectMemberDescriptor final
 {
-	enum class Type
-	{
-		Pointer,
-		Int32,
-		Double,
-	};
-	Type type;
-	std::size_t arrayLength;
-	char Object::*member;
-	Object *&getPointer(Object *object, std::size_t index) const
-	{
-		assert(type == Type::Pointer);
-		assert(index < arrayLength || (arrayLength == 0 && index == 0));
-		return (object->*reinterpret_cast<Object *(Object::*)[1]>(member))[index];
-	}
-	std::int32_t &getInt32(Object *object, std::size_t index) const
-	{
-		assert(type == Type::Int32);
-		assert(index < arrayLength || (arrayLength == 0 && index == 0));
-		return (object->*reinterpret_cast<std::int32_t(Object::*)[1]>(member))[index];
-	}
-	double &getDouble(Object *object, std::size_t index) const
-	{
-		assert(type == Type::Double);
-		assert(index < arrayLength || (arrayLength == 0 && index == 0));
-		return (object->*reinterpret_cast<double(Object::*)[1]>(member))[index];
-	}
-	Object *getIfPointer(Object *object, std::size_t index) const
-	{
-		if(type != Type::Pointer)
-			return nullptr;
-		return getPointer(object, index);
-	}
+    enum class Type
+    {
+        ReferenceIndex, Int32, Double,
+    };
+    Type type;
+    std::size_t arrayLength;
+    std::size_t offset;
+    static std::size_t getTypeSize(Type type)
+    {
+        switch(type)
+        {
+        case Type::ReferenceIndex:
+            return sizeof(std::size_t);
+        case Type::Int32:
+            return sizeof(std::int32_t);
+        case Type::Double:
+            return sizeof(double);
+        }
+        assert(false);
+        return sizeof(std::int32_t);
+    }
+    static std::size_t getTypeAlignment(Type type)
+    {
+        switch(type)
+        {
+        case Type::ReferenceIndex:
+            return alignof(std::size_t);
+        case Type::Int32:
+            return alignof(std::int32_t);
+        case Type::Double:
+            return alignof(double);
+        }
+        assert(false);
+        return alignof(std::int32_t);
+    }
+    std::size_t &getReferenceIndex(Object *object, std::size_t index) const
+    {
+        assert(type == Type::ReferenceIndex);
+        assert(index < arrayLength || (arrayLength == 0 && index == 0));
+        return reinterpret_cast<std::size_t *>(reinterpret_cast<char *>(object) + offset)[index];
+    }
+    std::int32_t &getInt32(Object *object, std::size_t index) const
+    {
+        assert(type == Type::Int32);
+        assert(index < arrayLength || (arrayLength == 0 && index == 0));
+        return reinterpret_cast<std::int32_t *>(reinterpret_cast<char *>(object) + offset)[index];
+    }
+    double &getDouble(Object *object, std::size_t index) const
+    {
+        assert(type == Type::Double);
+        assert(index < arrayLength || (arrayLength == 0 && index == 0));
+        return reinterpret_cast<double *>(reinterpret_cast<char *>(object) + offset)[index];
+    }
+    std::size_t getIfReferenceIndex(Object *object, std::size_t index) const
+    {
+        if(type != Type::ReferenceIndex)
+            return 0;
+        return getReferenceIndex(object, index);
+    }
+    ObjectMemberDescriptor
 };
 
 class Allocator;
 
 struct ObjectDescriptorBase
 {
-	std::vector<ObjectMemberDescriptor> members;
+    std::size_t size;
+    std::vector<ObjectMemberDescriptor> members;
+
 };
 
 class Allocator
